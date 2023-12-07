3c953797efe2cab736bfd922150b064e2213288e
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2023-12-07 23:14:41 +0600

    catagori section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero/Hero";
+import Catagories from "./components/Catagories";
 
 const App = () => {
     return (
         <div className="max-w-6xl mx-auto px-4 lg:px-0">
             <Navbar />
             <Hero />
+            <Catagories />
         </div>
     );
 };
